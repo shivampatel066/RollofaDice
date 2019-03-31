12c73922221baf6fe4af9882aa6d7a2eba56f92f
Shivam Patel <shivampatel066@gmail.com>
2019-03-31 22:27:52 +0530

    UI Completed First Commit With Out IB Connections ..

--- RollofaDice/ViewController.swift
+++ RollofaDice/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func randomDice(_ sender: Any) {
+        
+        let randomNumber = Int.random(in: 1...6)
+        label.text = String("Rolled a \(randomNumber)")
+        imageView.image = UIImage(named: "Dice\(randomNumber)")
+        
+    }
+    
 }
 
