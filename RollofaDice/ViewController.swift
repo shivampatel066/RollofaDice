dcbb3128f680dd7cddfd9cabd22cb66b777f3a14
Shivam Patel <shivampatel066@gmail.com>
2019-04-04 16:35:43 +0530

    Roll Dice single or double functionality added !!

--- RollofaDice/ViewController.swift
+++ RollofaDice/ViewController.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var imageView: UIImageView!
-    
-    
+   
     
 
     override func viewDidLoad() {
@@ -21,13 +18,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func randomDice(_ sender: Any) {
-        
-        let randomNumber = Int.random(in: 1...6)
-        label.text = String("Rolled a \(randomNumber)")
-        imageView.image = UIImage(named: "Dice\(randomNumber)")
-        
-    }
+   
     
 }
 
